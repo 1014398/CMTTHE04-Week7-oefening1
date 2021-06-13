77e96b1ce8550245620ef390d407a2ce6a07d09a
KokoDoko <erik.katerborg@gmail.com>
2021-06-13 22:15:04 +0200

    Update assignment

--- dev/game.ts
+++ dev/game.ts
@@ -1,47 +1,20 @@
-import { Background } from "./background.js"
-import { Enemy } from "./enemy.js"
-import { Player } from "./player.js"
-import { Speech } from "./speech.js"
-import { StartButton } from "./startbutton.js"
+import { GameObject } from "./gameobject.js"
+import { Robot } from "./robot.js"
+import { Startscreen } from "./startscreen.js"
 
 export class Game {
 
-    private background : Background
-    private player: Player
-    private speech : Speech
-    private enemies : Enemy[] = []
-    private startButton : StartButton
+    robot:Robot
 
-    constructor() {
-        this.speech = new Speech()
-        this.background = new Background()
-        this.startButton = new StartButton(this)
-    }
-
-    public startGame() {
-        this.player = new Player()
-        this.enemies.push(new Enemy(), new Enemy(), new Enemy())
-        
-        this.speech.speak("Eliminate all enemies")
+    constructor(){
+        this.robot = new Robot(this)
         this.gameLoop()
     }
 
-    private gameLoop() {
-        this.background.update()
-        this.player.update()
-        for(let e of this.enemies){
-            e.update()
-        }
-
-        requestAnimationFrame(() => this.gameLoop())
+    private gameLoop(){
+        this.robot.update()
+        requestAnimationFrame(()=>this.gameLoop())
     }
-
-    private checkCollision(a: ClientRect, b: ClientRect) {
-        return (a.left <= b.right &&
-            b.left <= a.right &&
-            a.top <= b.bottom &&
-            b.top <= a.bottom)
-     }
-} 
+}
 
 new Game()
